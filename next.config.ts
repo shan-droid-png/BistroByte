55ad5cb28de9596e70dfac23ffd497c92976fc03
Shanta Das <gxtreme59@gmail.com>
2025-07-26 19:54:05 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -27,6 +27,12 @@ const nextConfig: NextConfig = {
         hostname: 'images.unsplash.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.indianhealthyrecipes.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
